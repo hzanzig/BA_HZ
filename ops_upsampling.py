039baf925c73e42b682680dcb933d703f3769726
HZanzig <59019044+hzanzig@users.noreply.github.com>
2022-09-21 00:51:32 -0700

    Update ops_upsampling.py

--- ops_upsampling.py
+++ ops_upsampling.py
@@ -30,24 +30,24 @@ cur = conn.cursor()
 
 for i in range(0,len(worksheets3)):
     for j in range(2,worksheets3[i].max_row):
-        test=worksheets3[i]
-        ops_value = str(test[('A'+str(j))].value)
+        excelsheet=worksheets3[i]
+        ops_value = str(excelsheet[('A'+str(j))].value)
         try:
-            if str(test[('E'+str(j))].value) == '0':
+            if str(excelsheet[('E'+str(j))].value) == '0':
                 # check if the mapping was unsuccessful
                 print("sheet"+str(i+1)+","+str(j)+"/4,"+str(worksheets3[i].max_row))
                 ops_valuestrip=ops_value
                 upsampling = 0
-                x = 0
+                whileexit = 0
                 # shortening the ops code with every while iteration
-                while x ==0:
+                while whileexit ==0:
                     ops_valuestrip=ops_valuestrip[:-1]
                     query = "".join(['SELECT'," * FROM public.concept WHERE vocabulary_id LIKE 'OPS%' AND concept_code LIKE '", str(ops_valuestrip), "%'"])
                     #the sql query searches with a wildcard at the end of the stripped code in the database
                     cur.execute(query)
                     QueryErgebnis = cur.fetchall()
                     upsampling = upsampling+1
-                    x = cur.rowcount
+                    whileexit = cur.rowcount
                 opsKatalog =[]
                 #as many results can occur, which could corrupt the excel table format, the found catalogs will be reduced to the first 100.
                 if cur.rowcount > 100:
@@ -56,12 +56,12 @@ for i in range(0,len(worksheets3)):
                 else:
                     for z in range(0,cur.rowcount):
                         opsKatalog.append(QueryErgebnis[z][3])
-                toappend = [ops_value, test[('B'+str(j))].value, str(opsKatalog),cur.rowcount,upsampling,str(ops_valuestrip)]
+                toappend = [ops_value, excelsheet[('B'+str(j))].value, str(opsKatalog),cur.rowcount,upsampling,str(ops_valuestrip)]
                 ResultsDataframeops = ResultsDataframeops.append(pd.Series(toappend, index=ResultsDataframeops.columns[:len(toappend)]), ignore_index=True)
             else:
                 skipIf
         except:
-            toappend = [ops_value, test[('B'+str(j))].value,"NA","NA","NA","NA"]
+            toappend = [ops_value, excelsheet[('B'+str(j))].value,"NA","NA","NA","NA"]
             ResultsDataframeops = ResultsDataframeops.append(pd.Series(toappend, index=ResultsDataframeops.columns[:len(toappend)]), ignore_index=True)
 
 ResultsDataframeops.to_csv('OPS_upsampling.csv')
